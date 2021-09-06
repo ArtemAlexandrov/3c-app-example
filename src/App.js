8ee1da8f8d4a8cde95c27b40b0bf95e9af88fa47
Artem Alexandrov <svarog278@gmail.com>
2021-09-06 17:28:21 +0300

    rnd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import {PieChart} from 'react-minimal-pie-chart';
+import CryptoJS from 'crypto-js';
+import {useEffect, useState} from 'react'
+import { Card, Table, Badge } from 'antd';
+
+function getSig(path, secret) {
+  return CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, secret)
+    .update(path)
+    .finalize()
+    .toString(CryptoJS.enc.Hex);
+}
+const useData = (method, path, defaultData) => {
+  const [data, setData] = useState(defaultData)
+  const API_KEY = new URLSearchParams(window.location.search).get("API_KEY") || "";
+  const SECRET = new URLSearchParams(window.location.search).get("SECRET") || "";
+
+  useEffect(() => {
+    fetch(
+      `https://api.3commas.io${path}`,
+      {
+        method: method,
+        timeout: 30000,
+        agent: '',
+        headers: {
+          'APIKEY': API_KEY,
+          'Signature': getSig(path, SECRET)
+        }
+      }
+    ).then(response => response.json()).then(setData)
+  }, [path])
+  return data
+}
+
+const COLORS = [
+    "#23c6c8",
+    "#1c84c6",
+    "#f8ac59",
+    "#ed5565",
+    "#12b495",
+    "#a83bb1",
+    "#d26140",
+    "#4060e4",
+    "#ebdf33",
+    "#0b8ca1",
+    "#7740de",
+    "#1a9d42",
+    "#d63f70",
+    "#59a5f8",
+    "#7ddc33",
+    "#65349d",
+    "#aa831c",
+  ]
+
+const AccountChart = ({accountId}) => {
+  const balances = useData("POST", `/public/api/ver1/accounts/${accountId}/account_table_data`, [])
+return <Card title="Account" style={{width: 600}}>
+    <PieChart
+      data={
+        balances.map((balance, index) => ({
+          title: balance.currency_code,
+          value: balance.percentage,
+          color: COLORS[index % COLORS.length]
+        }))
+        }
+    />
+  <Table columns={[
+    {title: "Color",
+      dataIndex: 'percentage',
+      render: (text, row, index) => {
+      return <Badge
+        count={parseInt(text)}
+        style={{ backgroundColor: COLORS[index % COLORS.length] }} />
+      }
+    },
+    {
+    title: 'Currency',
+    dataIndex: 'currency_code',
+  }, {
+    title: '$',
+    dataIndex: 'usd_value',
+      align:"right",
+      render: (value) => parseFloat(value).toFixed(2)
+  }]} dataSource={balances} size="small" />
+  </Card>
+}
 
 function App() {
+  const accounts = useData("GET", "/public/api/ver1/accounts", []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {accounts.map(({id}) => <AccountChart accountId={id} key={id} />)}
     </div>
   );
 }
